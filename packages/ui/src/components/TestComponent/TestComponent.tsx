695fbea510da1e1fe17347c781954316506c2d21
James Kienle <james.kienle@gmail.com>
2023-11-06 19:45:22 -0800

    Remove original Test Component

--- packages/ui/src/components/TestComponent/TestComponent.tsx
+++
@@ -1,7 +0,0 @@
-export interface TestComponentProps {
-  name?: string;
-}
-
-export function TestComponent({ name = 'World' }: TestComponentProps) {
-  return <h1>Hello, {name}!</h1>;
-}
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,3 +1,1 @@
-// TODO: Delete me after we write our first real component
-export { TestComponent } from './TestComponent';
 export * from './Button';
