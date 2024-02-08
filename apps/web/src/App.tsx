dca317691cffbac65b66c029777d9ed086881449
Karin Hsu <khsu0720@gmail.com>
2024-02-08 13:21:34 -1000

    test input component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@intersectapi/ui';
+import { Input } from '@intersectapi/ui';
 
 function App() {
   return (
     <div>
       <h1>Hello, World!</h1>
-      <Button>Test Button!</Button>
+      <Button>Test Button!!!</Button>
+      <Input label="testinglabe" />
     </div>
   );
 }
